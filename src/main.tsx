732d19940e65fcda2616b4746f12230bb5a6ff3b
Mostafa Naeam <mnaeam10@gmail.com>
2026-01-22 04:53:47 +0200

    feat: Initialize React application with PrimeReact UI components and styling.

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,11 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
+import 'primereact/resources/themes/lara-light-indigo/theme.css'; // theme
+import 'primereact/resources/primereact.min.css';                 // core css
+import 'primeicons/primeicons.css';                               // icons
+import 'primeflex/primeflex.css';                                 // optional
+
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
